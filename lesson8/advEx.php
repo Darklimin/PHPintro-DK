86865aa0d4a41dd584ec4392bf39aa99f26af92f
Darius Kliminskas <dariuskliminskas@gmail.com>
2022-07-05 19:47:13 +0300

    3 tasks completed

--- lesson8/advEx.php
+++ lesson8/advEx.php
@@ -17,60 +17,72 @@ declare(strict_types=1);
 //}
 //
 //var_dump(exercise1('Hello_how_are-you doing?', [' ', '-', '_']));
-
-function exercise2(array $words): array
-{
-    /*
-    Sukategorizuokite žodžius pagal jų pradžios simbolį.
-    Funkcija kviečiama:
-    exercise2(['hello', 'Hickup', '123', 'computer'])
-    Funkcijos outputas:
-    [
-        'h' => ['hello', 'Hickup'],
-        '1' => ['123'],
-        'c' => ['computer'],
-    ]
+//
+//function exercise2(array $words): array
+//{
+//    /*
+//    Sukategorizuokite žodžius pagal jų pradžios simbolį.
+//    Funkcija kviečiama:
+//    exercise2(['hello', 'Hickup', '123', 'computer'])
+//    Funkcijos outputas:
+//    [
+//        'h' => ['hello', 'Hickup'],
+//        '1' => ['123'],
+//        'c' => ['computer'],
+//    ]
 //    */
 //    $output = [];
 //    $arrKeys = "";
-//    foreach ($words as $value){
+//    foreach ($words as $key => $value){
 //        $letter = substr($value, 0, 1);
 //        $arrKeys = strtolower($letter);
-//        $output[$arrKeys] = $value;
+//        $output[$arrKeys][$key] = $value;
 //    }
 //    return $output;
 //}
 //
-//var_dump(exercise2(['hello', 'Hickup', '123', 'computer']));
+//print_r(exercise2(['hello', 'Hickup', '123', 'computer']));
 
-//function exercise3(array $words): array
-//{
-//    /*
-//    Išveskite žodžių statistiką.
-//    Funkcija kviečiama:
-//    exercise2(['hello', 'you'])
-//    Funkcijos outputas:
-//    [
-//        'hello' => [
-//            'vowels' => 2,
-//            'consonants' => 3,
-//            'length' => 5,
-//            'starts_with' => h,
-//            'ends_with' => o,
-//        ],
-//        'you' => [
-//            'vowels' => 3,
-//            'consonants' => 0,
-//            'length' => 3,
-//            'starts_with' => y,
-//            'ends_with' => u,
-//        ]
-//    ]
-//    */
-//
-//    return [];
-//}
-//
+function exercise3(array $words): array
+{
+    /*
+    Išveskite žodžių statistiką.
+    Funkcija kviečiama:
+    exercise2(['hello', 'you'])
+    Funkcijos outputas:
+    [
+        'hello' => [
+            'vowels' => 2,
+            'consonants' => 3,
+            'length' => 5,
+            'starts_with' => h,
+            'ends_with' => o,
+        ],
+        'you' => [
+            'vowels' => 3,
+            'consonants' => 0,
+            'length' => 3,
+            'starts_with' => y,
+            'ends_with' => u,
+        ]
+    ]
+    */
+    $output = [];
+    foreach ($words as $key => $value){
+        $vow = substr_count($value, 'a')+substr_count($value,'e')+substr_count($value, 'i')
+            +substr_count($value, 'o')+substr_count($value, 'u')+substr_count($value, 'i')
+            +substr_count($value, 'y');
+        $output[$key] = ["$value" => [  "vowels" => $vow,
+                                        "consonants" => strlen($value) - $vow,
+                                        'length' => strlen($value),
+                                        'starts_with' => substr($value, 0, 1),
+                                        'ends_with' => substr($value, -1, 1)]];
+    }
+    return $output;
+}
+
+var_dump(exercise3(['hello', 'you']));
+
 //function exercise4(): array
 //{
 //    /*
