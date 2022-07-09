e5eab69c6929e562923331dc103c035c91acc935
Darius Kliminskas <dariuskliminskas@gmail.com>
2022-07-09 12:56:47 +0300

    Small changes

--- lesson9/dateTimeExample.php
+++ lesson9/dateTimeExample.php
@@ -1,1 +1,13 @@
 <?php
+
+// sukuriamas naujas objektas
+$date = new DateTime('2007-04-19 12:50');
+
+//objektas pamodifikuojamas
+$date->modify('-2days');
+
+//uzstatomas timezone
+$timeZone = 'America/Matamoros';
+$modifiedDate = $date->setTimezone(new DateTimeZone($timeZone));
+
+$modifiedDateWithFormat = $modifiedDate->format('Y.m.d H:i');
