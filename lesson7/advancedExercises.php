d3576f113fa48c6309f0d8a9fe8987b90d475c8e
Darius Kliminskas <dariuskliminskas@gmail.com>
2022-07-09 14:41:04 +0300

    3 ex completed

--- lesson7/advancedExercises.php
+++ lesson7/advancedExercises.php
@@ -27,27 +27,37 @@ function getShoppingCart(): array
 }
 // Visose užduotyse stenkitės naudoti array funkcijas
 
-function exercise1(): void
-{
-
-    /*
-    Išspausdinti visų krepšelio produktų pavadinimus vienoje eilutėje.
-    Comfy chair, Yellow lamp, Didzioji sofa, Softest rug, Blue shelf
-    */
-}
-
-function exercise2(): float
-{
-    /*
-    Suskaičiuokite pirkimų krepšelio bendrą sumą (price * quantity)
-    Kaip matote, krepšelio duomenys, kuriuos gavome iš svetimos sistemos, yra netvarkingi.
-    - Skaičiuojant reikėtų atsižvelgti tik į produktus, kurie turi laukus 'price' ir 'quantity'.
-    Jeigu produkto laukai užvadinti kitais pavadinimais arba iš viso jų neturi, tą produktą reikia ignoruoti.
-
-    */
-
-    return 0;
-}
+//function exercise1(): void
+//{
+//    echo implode(', ', array_keys(getShoppingCart()['products']));
+//    /*
+//    Išspausdinti visų krepšelio produktų pavadinimus vienoje eilutėje.
+//    Comfy chair, Yellow lamp, Didzioji sofa, Softest rug, Blue shelf
+//    */
+//}
+//
+//exercise1();
+//
+//function exercise2(): float
+//{
+//    /*
+//    Suskaičiuokite pirkimų krepšelio bendrą sumą (price * quantity)
+//    Kaip matote, krepšelio duomenys, kuriuos gavome iš svetimos sistemos, yra netvarkingi.
+//    - Skaičiuojant reikėtų atsižvelgti tik į produktus, kurie turi laukus 'price' ir 'quantity'.
+//    Jeigu produkto laukai užvadinti kitais pavadinimais arba iš viso jų neturi, tą produktą reikia ignoruoti.
+//    */
+//    $sum = 0;
+//    foreach (getShoppingCart()['products'] as $value){
+//        if (is_array($value)){
+//            if (array_key_exists('price', $value) && array_key_exists('quantity', $value)){
+//                $sum += $value['price'] * $value['quantity'];
+//            }
+//        }
+//    }
+//    return $sum;
+//}
+//
+//echo exercise2();
 
 function exercise3(): float
 {
@@ -62,89 +72,106 @@ function exercise3(): float
     Krepšelio nuolaida taikoma bendrai krepšelio sumai.
     Naudojama tik viena, didžiausia su krepšeliu susieta nuolaida ('cartDiscounts').
     */
-
-    return 0;
+    $sum = 0;
+    foreach (getShoppingCart()['products'] as $value){
+        $midSum = 0;
+        if (is_array($value)){
+            if (array_key_exists('price', $value) && array_key_exists('quantity', $value)){
+                $midSum += $value['price'] * $value['quantity'];
+            }
+            if (array_key_exists('discount', $value)){
+                $midSum -= $midSum / 100 * $value['discount'];
+            }
+        }
+        $sum += $midSum;
+    }
+    $finalDisc = getShoppingCart()['cartDiscounts'];
+    rsort($finalDisc);
+    $sum -= $sum / 100 * $finalDisc[0];
+    return round($sum, 2);
 }
 
-function exercise4(array $newIpList): array
-{
-    $existingIpList = [
-        '1.17.2.1',
-        '15.1.2.1',
-        '1.9.2.1',
-        '1.1.98.1',
-        '1.1.2.12',
-        '1.11.2.1',
-        '122.1.2.1',
-        '1.31.2.1',
-        '33.12.2.1',
-    ];
+echo exercise3();
 
-    /*
-    Sukategorizuokite ip adresų sąrašą.
-    ipsNew - ip iš $newIpList, kurių nėra $existingIpList
-    ipsOld - ip iš $existingIpList, kurių nėra $newIpList
-    ipsRemaining - ip, kurie egzistuoja abiejuose sąrašuose
-    funkcija butu kviečiam taip:
-    exercise4(
-        ['15.1.2.1', '16.1.8.1', '15.1.8.1']
-    );
-    */
-    $result = [
-        'ipsNew' => [],
-        'ipsOld' => [],
-        'ipsRemaining' => [],
-    ];
-
-    return $result;
-}
-
-function exercise5(): string
-{
-    $words = [
-        'over',
-        'jumps',
-        'fox',
-        'Quick',
-        'dog',
-        'lazy',
-        'very',
-        'the',
-    ];
-
-    /*
-    "Išverskite" masyvą į kitą pusę ir paverskite į string tipo reikšmę.
-    Arčiausiai vidurio esantys masyvo elementai turėtų atsirasti šonuose.
-    Masyvo elementų skaičius galėtų dideti, bet jis visada bus lyginis.
-    Rezultatas turėtų būti - 'Quick fox jumps over the very lazy dog'
-    */
-    return '';
-}
-
-
-/*
-    exercise 6
-    Parašykite savo array_map funkcijos versiją nesinaudodami pačia array_map funkcija
-*/
-function array_map_custom(callable $callback, array $array): array
-{
-    return [];
-}
-
-/*
-    exercise 7
-    Parašykite savo array_filter funkcijos versiją nesinaudodami pačia array_filter funkcija
-*/
-function array_filter_custom(array $array, ?callable $callback): array
-{
-    return [];
-}
-
-/*
-    exercise 8
-    Parašykite savo array_reduce funkcijos versiją nesinaudodami pačia array_reduce funkcija
-*/
-function array_reduce_custom(array $array, callable $callback, $carry)
-{
-    return 'array reduced to string';
-}
+//function exercise4(array $newIpList): array
+//{
+//    $existingIpList = [
+//        '1.17.2.1',
+//        '15.1.2.1',
+//        '1.9.2.1',
+//        '1.1.98.1',
+//        '1.1.2.12',
+//        '1.11.2.1',
+//        '122.1.2.1',
+//        '1.31.2.1',
+//        '33.12.2.1',
+//    ];
+//
+//    /*
+//    Sukategorizuokite ip adresų sąrašą.
+//    ipsNew - ip iš $newIpList, kurių nėra $existingIpList
+//    ipsOld - ip iš $existingIpList, kurių nėra $newIpList
+//    ipsRemaining - ip, kurie egzistuoja abiejuose sąrašuose
+//    funkcija butu kviečiam taip:
+//    exercise4(
+//        ['15.1.2.1', '16.1.8.1', '15.1.8.1']
+//    );
+//    */
+//    $result = [
+//        'ipsNew' => [],
+//        'ipsOld' => [],
+//        'ipsRemaining' => [],
+//    ];
+//
+//    return $result;
+//}
+//
+//function exercise5(): string
+//{
+//    $words = [
+//        'over',
+//        'jumps',
+//        'fox',
+//        'Quick',
+//        'dog',
+//        'lazy',
+//        'very',
+//        'the',
+//    ];
+//
+//    /*
+//    "Išverskite" masyvą į kitą pusę ir paverskite į string tipo reikšmę.
+//    Arčiausiai vidurio esantys masyvo elementai turėtų atsirasti šonuose.
+//    Masyvo elementų skaičius galėtų dideti, bet jis visada bus lyginis.
+//    Rezultatas turėtų būti - 'Quick fox jumps over the very lazy dog'
+//    */
+//    return '';
+//}
+//
+//
+///*
+//    exercise 6
+//    Parašykite savo array_map funkcijos versiją nesinaudodami pačia array_map funkcija
+//*/
+//function array_map_custom(callable $callback, array $array): array
+//{
+//    return [];
+//}
+//
+///*
+//    exercise 7
+//    Parašykite savo array_filter funkcijos versiją nesinaudodami pačia array_filter funkcija
+//*/
+//function array_filter_custom(array $array, ?callable $callback): array
+//{
+//    return [];
+//}
+//
+///*
+//    exercise 8
+//    Parašykite savo array_reduce funkcijos versiją nesinaudodami pačia array_reduce funkcija
+//*/
+//function array_reduce_custom(array $array, callable $callback, $carry)
+//{
+//    return 'array reduced to string';
+//}
