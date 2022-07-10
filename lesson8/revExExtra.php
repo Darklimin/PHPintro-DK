91cbff90d58d7603fe815cd0e90dcb6b17f74c99
Darius Kliminskas <dariuskliminskas@gmail.com>
2022-07-10 22:32:20 +0300

    Arguments from command line implemented

--- lesson8/revExExtra.php
+++ lesson8/revExExtra.php
@@ -2,37 +2,39 @@
 
 declare(strict_types=1);
 
-function exercise1 (string $myString):array {
-    $inventory = [
-        'apple' => [
-            'count' => 5,
-            'price' => 0.15,
-        ],
-        'carrot' => [
-            'count' => 100,
-            'price' => 0.01,
-        ],
-        'fish' => [
-            'count' => 15,
-            'price' => 5.5,
-        ],
-        'beer_bottle' => [
-            'count' => 22,
-            'price' => 1.3,
-        ],
-        'cheese' => [
-            'count' => 1,
-            'price' => 4.5,
-        ],
-        'wine_bottle' => [
-            'count' => 4,
-            'price' => 8,
-        ],
-        'bread' => [
-            'count' => 11,
-            'price' => 2.1,
-        ],
-    ];
+$inventory = [
+    'apple' => [
+        'count' => 5,
+        'price' => 0.15,
+    ],
+    'carrot' => [
+        'count' => 100,
+        'price' => 0.01,
+    ],
+    'fish' => [
+        'count' => 15,
+        'price' => 5.5,
+    ],
+    'beer_bottle' => [
+        'count' => 22,
+        'price' => 1.3,
+    ],
+    'cheese' => [
+        'count' => 1,
+        'price' => 4.5,
+    ],
+    'wine_bottle' => [
+        'count' => 4,
+        'price' => 8,
+    ],
+    'bread' => [
+        'count' => 11,
+        'price' => 2.1,
+    ],
+];
+
+function exercise1 (string $myString, array $myInventory): void
+{
     $firstEx = explode(" ", $myString);
     echo "You bought: ";
     for ($x=0; $x<count($firstEx); $x++){
@@ -44,13 +46,12 @@ function exercise1 (string $myString):array {
     $total = 0;
     echo PHP_EOL . "*****" . PHP_EOL;
     for ($y=0; $y<count($firstEx); $y++){
-        echo $secondEx[$y][0] . PHP_EOL . $inventory[$secondEx[$y][0]]["price"] .
-            " * " . $secondEx[$y][1] . " = " . $inventory[$secondEx[$y][0]]["price"] * $secondEx[$y][1] .
-        PHP_EOL;
-        $total += $inventory[$secondEx[$y][0]]["price"] * $secondEx[$y][1];
+        echo $secondEx[$y][0] . PHP_EOL . $myInventory[$secondEx[$y][0]]["price"] .
+            " * " . $secondEx[$y][1] . " = " . $myInventory[$secondEx[$y][0]]["price"] * $secondEx[$y][1] .
+            PHP_EOL;
+        $total += $myInventory[$secondEx[$y][0]]["price"] * $secondEx[$y][1];
     }
     echo "*****" . PHP_EOL . "Total: " . $total;
-    return $secondEx;
 }
 
-exercise1("apple:1 carrot:7 wine_bottle:2");
+exercise1($argv[1], $inventory);
