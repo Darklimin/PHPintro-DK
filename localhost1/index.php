462b2fe9e6326945f617be8b5b115dfc9aa04b81
Darius Kliminskas <dariuskliminskas@gmail.com>
2022-07-13 19:17:47 +0300

    Task 5 functionality implemented

--- localhost1/index.php
+++ localhost1/index.php
@@ -4,6 +4,8 @@
         <p>New TODO</p>
         </form><form method="POST" action="submit.php">
             <input type="text" name='todo' placeholder="Todo">
+            <input type=”date” name="my_date" placeholder="YYYY-MM-DD">
+            <input type=”time” name="my_time" placeholder="24:00">
             <input type="submit">
         </form>
     <div>
@@ -12,7 +14,8 @@
         $data = file_get_contents('./todolist.json');
         $todoArr = json_decode($data, true);
         foreach ($todoArr as $value){
-            echo $value . "<br>";
+            echo $value['todo'] . '    Created At: ' . $value['createdAt'] . "<br>";
+            echo 'Due date: ' . $value['dueDate'] . ' ' . $value['dueTime'] . '<br>';
         }
         ?>
     </div>
--- localhost1/submit.php
+++ localhost1/submit.php
@@ -4,9 +4,18 @@
 <?PHP
 $data = file_get_contents('./todolist.json');
 $todoArr = json_decode($data, true);
-$todoArr[] = $_POST['todo'];
+$date = date('Y-m-d H:i');
+$toDoDate = 0;
+$toDoTime = 0;
+$todoArr[] = [
+        'todo' => $_POST['todo'],
+        'createdAt' => $date,
+        'dueDate' => $_POST['my_date'],
+        'dueTime' => $_POST['my_time'],
+];
 $serializedData[] = json_encode($todoArr, JSON_PRETTY_PRINT);
 file_put_contents('./todolist.json', $serializedData);
 echo 'Form submitted!';?>
 </body>
 </html>
+
