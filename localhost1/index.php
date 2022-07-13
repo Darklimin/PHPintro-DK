8a0cf60240dbef0e52ddcb039a481545f4b22709
Darius Kliminskas <dariuskliminskas@gmail.com>
2022-07-13 21:22:03 +0300

    Date time updated - All completed

--- localhost1/index.php
+++ localhost1/index.php
@@ -5,8 +5,8 @@
         <legend>New TODO</legend>
         <form method="POST" action="./submit.php">
             <input type="text" name='todo' placeholder="Todo">
-            <input type=”date” name="my_date" placeholder="YYYY-MM-DD">
-            <input type=”time” name="my_time" placeholder="24:00">
+            <input type='date' name="my_date" placeholder="YYYY-MM-DD">
+            <input type='time' name="my_time" placeholder="24:00">
             <input type="submit">
         </form>
     </fieldset>
